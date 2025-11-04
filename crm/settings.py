8f3d6e0f8624e4b074ac4ef5e5a6c6c0c9ef212d
mferozmirza2005 <m.ferozmirza2005@gmail.com>
2025-11-04 15:37:28 +0500

    Update settings.py and static files.

--- crm/settings.py
+++ crm/settings.py
@@ -23,12 +23,12 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-52c34hl-kv2x!ic(7)a3i09$(1cgyda#6kl&3l3f*hw@%)t)lw")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
